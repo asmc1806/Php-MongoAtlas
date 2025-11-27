80d88ae211d016c8a797864fdf01f4d0ceebf9ef
asmc1806 <a-cmoreno@hotmail.com>
2025-11-26 19:27:22 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -5,19 +5,15 @@ use MongoDB\Client;
 
 $database = 'nivelacion_mongo';
 
-// URI normal de MongoDB Atlas
 $uri = "mongodb+srv://acmoreno_db_user:6G6ELgbF0CRzpKYK@cluster0.g56r7cd.mongodb.net/?retryWrites=true&w=majority";
 
 try {
-    // Conexión con parámetros TLS especiales para Railway / Render
     $client = new Client(
         $uri,
         [
-            'ssl' => true,
-            'tls' => true,
-            'tlsAllowInvalidHostnames' => true,
-            'tlsInsecure' => true,
-            'serverSelectionTimeoutMS' => 5000,
+            "tls" => true,
+            "tlsAllowInvalidHostnames" => true,  // Necesario para Railway/Render
+            "serverSelectionTimeoutMS" => 5000
         ]
     );
 
@@ -26,3 +22,5 @@ try {
 } catch (Exception $e) {
     die('Error conectando a MongoDB Atlas: ' . $e->getMessage());
 }
+
+
